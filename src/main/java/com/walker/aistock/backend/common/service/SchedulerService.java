c4059994d808e07ffe5dc96a904d001ffcf1fd31
minwoojeon <jeonminwoo0625@gmail.com>
2024-08-13 12:17:13 +0900

    FEAT : 스케쥴러 시간 조정 및 로그 추가

--- src/main/java/com/walker/aistock/backend/common/service/SchedulerService.java
+++ src/main/java/com/walker/aistock/backend/common/service/SchedulerService.java
@@ -7,12 +7,13 @@ import com.walker.aistock.backend.data.service.FearGreedService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Slf4j
 @Component
 @EnableScheduling
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -25,14 +26,25 @@ public class SchedulerService {
     StockRepository stockRepository;
 
     @Transactional
-    @Scheduled(cron = "0 53 11 * * ?")
+    @Scheduled(cron = "0 1 0 * * ?") // 매일 자정 +1분 - 해당 데이터가 없으면 main 화면 nullException
+    public void makeFearGreed() {
+
+        log.info("start makeFearGreed");
+        fearGreedService.saveFearGreed();
+        log.info("end makeFearGreed");
+
+    }
+
+    // TODO 1. DATA update 시점 2. 트래픽 한산한 시점 을 고려하여 시간 조정
+    @Transactional
+    @Scheduled(cron = "0 0 6 * * ?") // 매일 새벽 6시(예상 동작 시간 = 종목수*2분)
     public void makeTodayStockData() {
 
-        fearGreedService.saveFearGreed();
-
+        log.info("start makeTodayStockData");
         for (Stock stock : stockRepository.findAll()) {
             chatGPTService.chatGPTAnalysis(stock);
         }
+        log.info("end makeTodayStockData");
 
     }
 

55bce9b8550bf4b43ac69c9aab85f107525ddbd6
minwoojeon <jeonminwoo0625@gmail.com>
2025-02-18 20:59:23 +0900

    chore : 임시 배포

--- src/test/java/com/walker/aistock/AiStockApplicationTests.java
+++ src/test/java/com/walker/aistock/AiStockApplicationTests.java
@@ -3,6 +3,7 @@ package com.walker.aistock;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// temp
 @SpringBootTest
 class AiStockApplicationTests {
 
